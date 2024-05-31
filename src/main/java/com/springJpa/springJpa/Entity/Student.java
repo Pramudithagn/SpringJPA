b3cfb37637472a5f989a36f83928f6006b5ce9c4
Pramuditha <pramudithagn@gmail.com>
2024-05-31 22:24:37 +0530

    initial

---
+++ src/main/java/com/springJpa/springJpa/Entity/Student.java
@@ -0,0 +1,45 @@
+package com.springJpa.springJpa.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(
+        name = "student_table",
+        uniqueConstraints = @UniqueConstraint(
+                name = "unique_email",
+                columnNames = "email_address"
+        )
+)
+public class Student {
+
+    @Id
+    @SequenceGenerator(
+            name = "id_sequence",
+            sequenceName = "id_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "id_sequence"
+    )
+    private Long studentId;
+    @Column(
+            name = "email_address",
+            nullable = false
+    )
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String guardianName;
+    private String guardianMobile;
+    private String guardianEmail;
+
+}
